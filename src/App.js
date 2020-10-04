a12a129ae3bbfe5ea28cd3fb6886be2e2658df64
PatrickBornay <patrick.bornay@gmail.com>
2020-10-05 10:34:55 +1100

    added todoform functional component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import './App.css';
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        className='input'
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
 function Todo({ todo }) {
   return <div className='todo'>{todo.text}</div>;
 }
